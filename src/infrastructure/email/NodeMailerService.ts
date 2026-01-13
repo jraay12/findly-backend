d1b907e99cb3bfa085b760904a1b08066ca61264
John Ray <johnraycaete@gmail.com>
2026-01-13 10:43:03 +0800

    Practice

--- src/infrastructure/email/NodeMailerService.ts
+++ src/infrastructure/email/NodeMailerService.ts
@@ -6,8 +6,6 @@ export class NodemailerService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       service: "gmail",
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
